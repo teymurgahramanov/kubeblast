8d6aa74042bbe937cff2fa8e618cf741d45ddcf3
Teymur <teymur_gahramanov@gmail.com>
2024-12-31 13:20:13 +0400

    upd

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
-from app.routes import jmeter, logs, reports
 
 app = FastAPI()
 
-app.include_router(jmeter.router, prefix="/task", tags=["JMeter"])
-app.include_router(logs.router, prefix="/logs", tags=["Logs"])
-app.include_router(reports.router, prefix="/reports", tags=["Reports"])
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
