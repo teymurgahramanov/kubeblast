535de98e11bc399e414d1ede05bc186baa701470
Teymur Gahramanov <teymur_gahramanov@outlook.com>
2025-01-08 12:07:42 +0400

    upd

--- app/routers/jobs.py
+++ app/routers/jobs.py
@@ -1,13 +1,15 @@
+import os
 import yaml
-from fastapi import APIRouter
-from app.dependencies import kubernetes_client_batch as kubernetes_client
+from jinja2 import Template
+from fastapi import APIRouter, UploadFile, File
+from app.dependencies import kubernetes_client_batch, kubernetes_client_core
 
 router = APIRouter()
 namespace = "default"
 
 @router.get("/jobs")
 async def get_jobs():
-    jobs = kubernetes_client.list_namespaced_job(namespace=namespace)
+    jobs = kubernetes_client_batch.list_namespaced_job(namespace=namespace)
     return {"jobs": [job.metadata.name for job in jobs.items]}
 
 @router.get("/jobs/{job}")
@@ -15,10 +17,32 @@ async def get_job(job: str):
     return {"job": job}
 
 @router.post("/jobs")
-async def create_job():
-    with open("/app/app/job.yaml", 'r') as file:
-        job_manifest = yaml.safe_load(file)
-    job = kubernetes_client.create_namespaced_job(
+async def create_job(config_file: UploadFile = File(...)):
+    file_content = await config_file.read()
+    file_name = "plan.jmx"
+    configmap_template_path = os.path.join(os.path.dirname(__file__), "../templates/configmap.yaml.j2")
+    with open(configmap_template_path, 'r') as file:
+        configmap_template_content = file.read()
+    rendered_configmap = Template(configmap_template_content).render(
+        configmap_name=f"{job_name}-configmap",
+        namespace=namespace,
+        data={file_name: file_content.decode("utf-8")}
+    )
+    configmap_manifest = yaml.safe_load(rendered_configmap)
+    kubernetes_client_core.create_namespaced_config_map(
+        namespace=namespace,
+        body=configmap_manifest
+    )
+    job_template_path = os.path.join(os.path.dirname(__file__), "../templates/job.yaml.j2")
+    with open(job_template_path, 'r') as file:
+        job_template_content = file.read()
+    rendered_job = Template(job_template_content).render(
+        job_name=job_name,
+        namespace=namespace,
+        configmap_name=f"{job_name}-configmap"
+    )
+    job_manifest = yaml.safe_load(rendered_job)
+    job = kubernetes_client_batch.create_namespaced_job(
         namespace=namespace,
         body=job_manifest
     )
@@ -26,7 +50,7 @@ async def create_job():
 
 @router.delete("/jobs/{job}")
 async def get_job(job: str):
-    job = kubernetes_client.delete_namespaced_job(
+    job = kubernetes_client_batch.delete_namespaced_job(
         namespace=namespace,
         name  = job
     )
