f49747a85d91f44ecfc320e685b3fef1e1d9d00c
Teymur Gahramanov <teymur_gahramanov@outlook.com>
2025-01-08 16:20:12 +0400

    upd

--- app/routers/jobs.py
+++ app/routers/jobs.py
@@ -1,7 +1,7 @@
 import os
 import yaml
 from jinja2 import Template
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, HTTPException, UploadFile, File, Header
 from app.dependencies import kubernetes_client_batch, kubernetes_client_core
 
 router = APIRouter()
@@ -17,19 +17,31 @@ async def get_job(job: str):
     return {"job": job}
 
 @router.post("/jobs")
-async def create_job(config_file: UploadFile = File(...)):
+async def create_job(plan_file: UploadFile = File(...),project_name: str = Header(None)):
+
+    if not project_name:
+        raise HTTPException(status_code=400, detail="PROJECT_NAME header is required")
+    job_name = project_name
+
     # Generate ConfigMap
-    file_content = await config_file.read()
-    file_name = "plan.jmx"
+    try:
+        file_content = await plan_file.read()
+        file_name = "plan.jmx"
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Failed to read uploaded file: {str(e)}")
+    
     configmap_template_path = os.path.join(os.path.dirname(__file__), "../templates/configmap.yaml.j2")
+
     with open(configmap_template_path, 'r') as file:
         configmap_template_content = file.read()
     rendered_configmap = Template(configmap_template_content).render(
-        configmap_name=f"{job_name}-configmap",
+        name=job_name,
         namespace=namespace,
         data={file_name: file_content.decode("utf-8")}
     )
+
     configmap_manifest = yaml.safe_load(rendered_configmap)
+
     kubernetes_client_core.create_namespaced_config_map(
         namespace=namespace,
         body=configmap_manifest
@@ -40,9 +52,8 @@ async def create_job(config_file: UploadFile = File(...)):
     with open(job_template_path, 'r') as file:
         job_template_content = file.read()
     rendered_job = Template(job_template_content).render(
-        job_name=job_name,
+        name=job_name,
         namespace=namespace,
-        configmap_name=f"{job_name}-configmap"
     )
     job_manifest = yaml.safe_load(rendered_job)
     job = kubernetes_client_batch.create_namespaced_job(
