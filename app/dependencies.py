7ada31fb60396fde5970b26794b5c06862c8a8ad
Teymur Gahramanov <teymur_gahramanov@outlook.com>
2025-01-09 18:42:58 +0400

    asdfa

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,12 +1,31 @@
+import os
+import yaml
+from jinja2 import Template
 from kubernetes import client, config
 
-def get_kubernetes_client_batch():
-  config.load_incluster_config()
-  return client.BatchV1Api()
+config.load_incluster_config()
+namespace = "default"
 
-def get_kubernetes_client_core():
-  config.load_incluster_config()
-  return client.CoreV1Api()
+def create_job(job_name,file_name,file_content):
 
-kubernetes_client_batch = get_kubernetes_client_batch()
-kubernetes_client_core = get_kubernetes_client_core()
+  metadata = client.V1ObjectMeta(name=job_name, namespace=namespace)
+
+  client.CoreV1Api().create_namespaced_config_map(
+    metadata=metadata,
+    body=client.V1ConfigMap(data={file_name: file_content.decode("utf-8")})
+  )
+
+  # Generate Job
+  job_template_path = os.path.join(os.path.dirname(__file__), "../job.yaml.j2")
+  with open(job_template_path, 'r') as file:
+      job_template_content = file.read()
+  rendered_job = Template(job_template_content).render(
+      name=job_name,
+      namespace=namespace,
+      file_name=file_name
+  )
+  job_manifest = yaml.safe_load(rendered_job)
+  job = client.BatchV1Api().create_namespaced_job(
+      body=job_manifest
+  )
+  return {"job": {job.metadata.name}}
--- app/routers/jobs.py
+++ app/routers/jobs.py
@@ -2,7 +2,7 @@ import os
 import yaml
 from jinja2 import Template
 from fastapi import APIRouter, HTTPException, UploadFile, File, Header
-from app.dependencies import kubernetes_client_batch, kubernetes_client_core
+from app.dependencies import create_job
 
 router = APIRouter()
 namespace = "default"
@@ -23,49 +23,24 @@ async def create_job(plan_file: UploadFile = File(...),project_name: str = Heade
         raise HTTPException(status_code=400, detail="PROJECT_NAME header is required")
     job_name = project_name
 
+    if plan_file.content_type != "text/plain":
+        raise HTTPException(status_code=400, detail="Invalid file type. Expected text/plain")
+    
     # Generate ConfigMap
     try:
-        file_content = await plan_file.read()
         file_name = "plan.jmx"
+        file_content = await plan_file.read()
+        file_content_decoded = file_content.decode("utf-8")
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Failed to read uploaded file: {str(e)}")
-    
-    configmap_template_path = os.path.join(os.path.dirname(__file__), "../templates/configmap.yaml.j2")
-
-    with open(configmap_template_path, 'r') as file:
-        configmap_template_content = file.read()
-    rendered_configmap = Template(configmap_template_content).render(
-        name=job_name,
-        namespace=namespace,
-        data={file_name: file_content.decode("utf-8")}
-    )
-
-    configmap_manifest = yaml.safe_load(rendered_configmap)
-
-    kubernetes_client_core.create_namespaced_config_map(
-        namespace=namespace,
-        body=configmap_manifest
-    )
-
-    # Generate Job
-    job_template_path = os.path.join(os.path.dirname(__file__), "../templates/job.yaml.j2")
-    with open(job_template_path, 'r') as file:
-        job_template_content = file.read()
-    rendered_job = Template(job_template_content).render(
-        name=job_name,
-        namespace=namespace,
-    )
-    job_manifest = yaml.safe_load(rendered_job)
-    job = kubernetes_client_batch.create_namespaced_job(
-        namespace=namespace,
-        body=job_manifest
-    )
-    return {"job": {job.metadata.name}}
+    return create_job(job_name, file_name, file_content)
 
 @router.delete("/jobs/{job}")
 async def get_job(job: str):
     job = kubernetes_client_batch.delete_namespaced_job(
         namespace=namespace,
-        name  = job
+        name  = job,
+        orphan_dependents = True,
+        propagation_policy = 'Foreground'
     )
     return {"job": "deleted"}
