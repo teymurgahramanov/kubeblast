5556eba185337250dadc14f5d83ec1560ddcc289
Teymur <teymur_gahramanov@gmail.com>
2025-06-01 22:45:44 +0400

    upd

--- api/config.py
+++ api/config.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
+
+    # General
     IS_PRO = False
 
     LOG_LEVEL: str = os.getenv("LOG_LEVEL", "ERROR")
@@ -16,6 +18,7 @@ class Config:
     PER_USER_CURRENT_JOBS_LIMIT: int = int(os.getenv("PER_USER_CURRENT_JOBS_LIMIT", 3))
     WORKER_WATCH_INTERVAL: int = 3
 
+    # Database
     MONGO_HOST: str = os.getenv("MONGO_HOST", "localhost")
     MONGO_PORT: int = int(os.getenv("MONGO_PORT", 27017))
     MONGO_DB_USER: str = os.getenv("MONGO_DB_USER", "kubeblast")
@@ -23,6 +26,7 @@ class Config:
     MONGO_DB_NAME: str = os.getenv("MONGO_DB_NAME", "kubeblast")
     MONGO_URI: str = f"mongodb://{MONGO_DB_USER}:{MONGO_DB_PASS}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB_NAME}"
 
+    # Storage
     STORAGE_BACKEND: str = os.getenv("STORAGE_BACKEND", "fs") # fs, s3
     STORAGE_DIR: str = "/data"
     STORAGE_PVC_NAME: str = os.getenv("STORAGE_PVC_NAME")
@@ -33,6 +37,7 @@ class Config:
     S3_REGION: str = os.getenv("S3_REGION", "us-east-1")
     S3_BUCKET: str = os.getenv("S3_BUCKET")
     
+    # Job
     K8S_JOB_IMAGE: str = os.getenv("K8S_JOB_IMAGE", "alpine/jmeter:5.6")
     K8S_JOB_HELPER_IMAGE_S3: str = os.getenv("K8S_JOB_HELPER_IMAGE_S3", "amazon/aws-cli:2.27.12")
     K8S_JOB_HELPER_IMAGE_FS: str = os.getenv("K8S_JOB_HELPER_IMAGE_FS", "alpine:3.18")
@@ -79,14 +84,16 @@ class Config:
             K8S_JOB_RESOURCES = None
 
     # LDAP Configuration
-    LDAP_ENABLED: bool = os.getenv("LDAP_ENABLED", "false")
+    LDAP_ENABLED: bool = os.getenv("LDAP_ENABLED", "false").lower() == "true"
     LDAP_SERVER: str = os.getenv("LDAP_SERVER")
     LDAP_BASE_DN: str = os.getenv("LDAP_BASE_DN")
     LDAP_BIND_DN: str = os.getenv("LDAP_BIND_DN")
-    LDAP_BIND_PASSWORD: str = os.getenv("LDAP_BIND_PASSWORD", "")
+    LDAP_BIND_PASSWORD: str = os.getenv("LDAP_BIND_PASSWORD")
     LDAP_USER_SEARCH_FILTER: str = os.getenv("LDAP_USER_SEARCH_FILTER", "(&(objectClass=person)(sAMAccountName={username}))")
     LDAP_GROUP_SEARCH_FILTER: str = os.getenv("LDAP_GROUP_SEARCH_FILTER", "(&(objectClass=group)(member={dn}))")
-    LDAP_USER_ATTRIBUTES: list = ["uid", "sAMAccountName", "cn", "mail", "memberOf"]
-    LDAP_GROUP_ATTRIBUTES: list = ["cn", "member"]
+    LDAP_USER_ATTRIBUTES: list = os.getenv("LDAP_USER_ATTRIBUTES", "uid,sAMAccountName,cn,mail,memberOf").split(",")
+    LDAP_GROUP_ATTRIBUTES: list = os.getenv("LDAP_GROUP_ATTRIBUTES", "cn,member").split(",")
+    LDAP_USE_TLS: bool = os.getenv("LDAP_USE_TLS", "false").lower() == "true"
+    LDAP_VERIFY_CERT: bool = os.getenv("LDAP_VERIFY_CERT", "true").lower() == "true"
 
 config = Config()
